2d3852996cf61c37f32d2994b17d07ae96bb1b03
anuragshrivastava-simformsolutions <anurag.s@simformsolutions.com>
2020-06-01 01:38:03 +0530

    a

---
+++ qqq.js
@@ -0,0 +1,7 @@
+remote:
+remote: Create a pull request for 'one' on GitHub by visiting:
+remote:      https://github.com/anuragshrivastava-simformsolutions/ooo/pull/new/one
+remote:
+To https://github.com/anuragshrivastava-simformsolutions/ooo.git
+ * [new branch]      one -> one
+Branch 'one' set up to track remote branch 'one' from 'origin'.
